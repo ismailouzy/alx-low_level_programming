1f3af50a116bea22d23a69b3fa5c034a7a0589dc
ismailouzy <ismailouzal@gmail.com>
2023-10-06 14:24:01 -0700

    numbers with int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+# include <stdlib.h>
+# include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int az  = 48;
+
+	while (az <= 57)
+	{
+		putchar(az);
+		az++;
+	}
+	putchar('\n');
+	return (0);
+}
