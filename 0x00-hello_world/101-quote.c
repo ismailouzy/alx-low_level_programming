9f5bc958ea4493dfbdcf63668603a0899bbf564e
ismailouzy <ismailouzal@gmail.com>
2023-10-05 17:28:28 -0700

    advanced tasks

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Using stdlib library to print a sentence without using printf or puts
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	system("echo'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
