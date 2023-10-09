c85e615d31a4b2f0cc97f5eae8b714d0a056bba3
ismailouzy <ismailouzal@gmail.com>
2023-10-09 10:22:00 -0700

    priting all possible different combs

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+	int
+main()
+{
+	int a, b;
+
+	for (a = '0'; a < '9'; a++)
+		for (b = a + 1; b <= '9'; b++)
+		{
+
+			{
+
+				putchar (a);
+				putchar (b);
+
+				if (a != '8' || (a != '8' && b != '9'))
+					putchar (',');
+				putchar (' ');
+			}
+
+
+		}
+
+	return (0);
+}
+
