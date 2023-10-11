28905f86f948901c7cce12296530240640df818f
ismailouzy <ismailouzal@gmail.com>
2023-10-11 14:11:49 -0700

    fixed times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,43 +1,37 @@
-#include <unistd.h>
 #include "main.h"
-
 /**
  * times_table - entry point
  *
  * Return: Always 0 (Success)
  */
-
 void times_table(void)
 {
-	int a, b, ab;
+	int a, b;
 
-	for (a = 0; a <= 9; ++a)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; ++b)
+		for (b = 0; b <= 9; b++)
 		{
-			ab = a * b;
+			int ab = (a * b);
 
 			if (b > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-
 			if (ab >= 10)
 			{
 				_putchar((ab / 10) + '0');
 				_putchar((ab % 10) + '0');
 			}
-			else
+			else if (ab < 10 && b > 0)
 			{
-				if (b > 0)
 				_putchar(' ');
 				_putchar((ab % 10) + '0');
 			}
-
+			else
+				_putchar((ab % 10) + '0');
 		}
-
-		putchar('\n');
+		_putchar('\n');
 	}
 }
-
