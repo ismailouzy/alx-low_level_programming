8c440d0c16b972f916181961bbd56ed7bed886fc
ismailouzy <ismailouzal@gmail.com>
2023-10-10 17:00:57 -0700

    alphabets x10 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * print alphabet - entry point
+ * print_alphabet_x10 - entry point
  *
  *
  *
- *  Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet_x10(void)
