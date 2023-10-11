0b1eddc2924a88e5cf43ce4e90e105e17b1c9d5b
ismailouzy <ismailouzal@gmail.com>
2023-10-10 20:46:37 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ * @a: a parametre
+ * @b: b parametre
+ *
+ * Return: ab (success)
+ */
+
+int add(int a, int b)
+{
+	int ab = a + b;
+
+	return (ab);
+
+}
