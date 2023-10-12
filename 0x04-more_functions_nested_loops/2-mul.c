d4fdba0a8af2b420e6895c9430fa83de866d5537
ismailouzy <ismailouzal@gmail.com>
2023-10-12 08:35:03 -0700

    multiply two n

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - entry point
+ *
+ * @a: a parametre
+ * @b: b parametre
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	int ab;
+
+	ab = a * b;
+
+	return (ab);
+
+}
