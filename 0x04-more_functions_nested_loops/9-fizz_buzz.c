aef76ecf0a8ddcceac10d6e6dbbb7d5c6725fbfe
ismailouzy <ismailouzal@gmail.com>
2023-10-12 16:43:22 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,21 +14,26 @@ int main(void)
 	{
 		if (a % 15 == 0)
 
-			printf("FizzBuzz");
+			printf(" FizzBuzz");
 
 		else if (a % 5 == 0)
 
-			printf("Buzz");
+			printf(" Buzz");
 
 		else if (a % 3 == 0)
 
-			printf("Fizz");
+			printf(" Fizz");
 
-		else
+		else if (a == 1)
 
 			printf("%d", a);
 
-		printf(" ");
+		else
+
+			printf(" %d", a);
+
+
 	}
+
 	return (0);
 }
