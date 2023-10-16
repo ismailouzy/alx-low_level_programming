7bee8ec11916a43c1de92aec8171cc53883ad1b0
ismailouzy <ismailouzal@gmail.com>
2023-10-16 16:21:46 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ *
+ * @str: str parametre
+ *
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+
+}
