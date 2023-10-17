bdb514fad471752ec4471c503d1fe5441c34e78f
ismailouzy <ismailouzal@gmail.com>
2023-10-16 17:36:46 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ *
+ * @str: str parametre
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		if (n % 2 == 0)
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
