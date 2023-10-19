6de5b31507a14df6ebe90516bbb46c95068e0d88
ismailouzy <ismailouzal@gmail.com>
2023-10-19 07:57:20 -0700

    concatenating two strings without library function

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - Entry point
+ *
+ * @dest: dest parametre
+ * @src: src parametre
+ *
+ * Return: Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int a, b;
+
+	for (a = 0; dest[a] != '\0' ; a++)
+	{
+	}
+	for (b = 0; src[b] != '\0'; b++)
+	{
+		dest[a + b] = src[b];
+	}
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme file for 'More pointers, arrays and strings' c project
