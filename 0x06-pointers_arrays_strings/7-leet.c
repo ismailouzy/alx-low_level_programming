470d6a864103621d429e0bacb71bf8ce127c4e74
ismailouzy <ismailouzal@gmail.com>
2023-10-19 19:23:17 -0700

    1337 econding

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * leet - Entry point
+ *
+ * @s: s parametre
+ *
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+	int i;
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (s[i] == a[j] || s[i] == a[j] - 32)
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
