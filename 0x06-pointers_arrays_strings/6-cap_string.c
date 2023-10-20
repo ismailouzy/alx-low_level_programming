6d17104bd46d1ece4dd91930af574b774d90bd0d
ismailouzy <ismailouzal@gmail.com>
2023-10-19 17:27:14 -0700

    change the first letter of each string to an uppercase

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string - Entry point
+ *
+ * @s: s parametre
+ *
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+
+	int i;
+
+	for (i = 0; s[i]; i++)
+
+	{
+		if (s[i] == '\t')
+		{
+
+			s[i] = ' ';
+		}
+		if (i == 0)
+
+		{
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+
+			{
+				s[i] = s[i] - 32;
+
+			}
+
+		}
+		else if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '.')
+		{
+
+
+			if (s[i] >= 'a' && s[i] <= 'z')
+			{ s[i] = s[i] - 32;
+			}
+		}
+
+	}
+	return (s);
+}
