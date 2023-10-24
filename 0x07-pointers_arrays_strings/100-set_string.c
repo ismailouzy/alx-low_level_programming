3cf4a5c1729a3d60bbf1ef97397038fb6a521fba
ismailouzy <ismailouzal@gmail.com>
2023-10-23 17:21:39 -0700

    bob dylan

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - entry point
+ *
+ * @s: s parametre
+ * @to: to parametre
+ *
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
