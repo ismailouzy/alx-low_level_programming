b3147b980c1f3ff3f14cd24430f4a35a1b18284b
ismailouzy <ismailouzal@gmail.com>
2023-10-27 11:56:01 -0700

    hello world argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: argc parametre
+ * @argv: argv parametre
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
