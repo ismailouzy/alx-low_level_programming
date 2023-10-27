e42008395d808294d2de47cd0b8658dd9cd32126
ismailouzy <ismailouzal@gmail.com>
2023-10-27 12:15:16 -0700

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argc parametre
+ * @argv: argv parametre
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int multi;
+
+	if (argc != 3)
+	{
+		printf("Error");
+
+		return (1);
+	}
+
+	else
+	{
+
+		multi = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", multi);
+	}
+	return (0);
+}
