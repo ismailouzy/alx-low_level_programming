a44b262fdda28170176119db9f30e17c98650d64
ismailouzy <ismailouzal@gmail.com>
2023-11-05 07:19:46 -0800

    defining a structure for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
-#define dog { \
-	char *name; \
-	float age; \
-	char *owner; \
-}
+/**
+ * struct dog - Structure for dog
+ * @name: name parametre
+ * @age: age parametre
+ * @owner: owner parametre
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif
