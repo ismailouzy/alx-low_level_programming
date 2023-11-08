a9de6999277ab4408173b5c8c1998c400b4ae55a
ismailouzy <ismailouzal@gmail.com>
2023-11-08 08:11:49 -0800

    callback to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_name - function that allocates memory for an array with calloc
+ *
+ * @name: name ptr parametre
+ * @f: f function ptr parametre
+ *
+ * Return: success.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
