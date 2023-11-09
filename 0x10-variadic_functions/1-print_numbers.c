6cd6e0e35ec129ca3a1fe73e94b01c867cd25beb
ismailouzy <ismailouzal@gmail.com>
2023-11-09 14:24:23 -0800

    vardiac function that print nums

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator != NULL)
+	if (separator != NULL && !n)
 	{
 
 		for (i = 0; i < n; i++)
