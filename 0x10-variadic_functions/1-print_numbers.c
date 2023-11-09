6363b9204b809e0fa1ce1da1ab90bc58fdbb03af
ismailouzy <ismailouzal@gmail.com>
2023-11-09 14:28:25 -0800

    vardiac function that print nums

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(args, n);
 
-	if (separator != NULL || !n)
+	if (!n)
 	{
 
 		for (i = 0; i < n; i++)
@@ -27,7 +27,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			num = va_arg(args, int);
 
 			printf("%d", num);
-			if (i < n - 1)
+			if (i < n - 1 && separator != NULL)
 				printf("%s", separator);
 
 		}
