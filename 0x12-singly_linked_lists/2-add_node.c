e67d1d3f6034ed5951d2c203de8a85c2cdbe7c39
ismailouzy <ismailouzal@gmail.com>
2023-11-16 16:25:11 -0800

    at the beginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning
+ *
+ * @str: str parametre
+ * @head: parametre
+ *
+ * Return: Success
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (head == NULL || new == NULL)
+	{
+		return (NULL);
+	}
+	if (str != NULL)
+		new->str = strdup(str);
+
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
