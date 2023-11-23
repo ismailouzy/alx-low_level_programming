e989476e3b229ddfbf962c61dcf291226234e67c
ismailouzy <ismailouzal@gmail.com>
2023-11-23 12:13:57 -0800

    binarty to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary to an unsigned int
+ *
+ * @b: b parametre
+ *
+ * Return: Success
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+		return (0);
+
+	unsigned int dec = 0;
+	int i = 0;
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+
+		dec <<= 1;
+		dec += (b[i] - '0');
+		i++;
+	}
+
+	return (dec);
+}
