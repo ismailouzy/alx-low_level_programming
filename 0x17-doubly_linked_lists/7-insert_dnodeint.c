a5a76ee4e85d6950d068778226d3b61ef8f48fff
ismailouzy <ismailouzal@gmail.com>
2024-01-11 07:07:06 -0800

    insert at nth

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserting a node at index
+ * @h: the pointer to the head of the node
+ * @idx: the index of the node
+ * @n: data of the node
+ * Return: Success
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *ptr;
+	dlistint_t *new;
+
+	ptr = *h;
+
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (idx != 1)
+	{
+		ptr = ptr->next;
+		idx--;
+	}
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+
+
+	if (ptr->next != NULL)
+		new->next = ptr->next;
+	else
+		new->next = NULL;
+	new->prev = ptr;
+	ptr->next = new;
+
+
+	return (new);
+}
