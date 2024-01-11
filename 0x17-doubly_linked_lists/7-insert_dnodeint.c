c5ecfc6716ec19f4b9b36e5c2cd0f79e2abfa5e4
ismailouzy <ismailouzal@gmail.com>
2024-01-11 07:53:30 -0800

    don

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,7 +34,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 
 	if (ptr->next != NULL)
-		new->next = ptr->next->prev;
+		new = ptr->next->prev;
 	new->next = ptr->next;
 	new->prev = ptr;
 	ptr->next = new;
