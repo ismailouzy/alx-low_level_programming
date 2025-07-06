e345585f087fad2ef261a83f476e8d6959b852e1
ismailouzy <ismailouzal@gmail.com>
2025-07-06 14:32:00 +0100

    islower - betty fixed

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - writes a function in lowercase
+ * @c: the asci value
  *
  * Return: on success 1, in errno 0
  */
@@ -8,7 +9,7 @@
 int _islower(int c)
 {
 	int i;
-	
+
 	for (i = 97; i < 123; i++)
 	{
 		if (i == c)
