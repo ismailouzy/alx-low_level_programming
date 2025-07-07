f62bd917ffde4fc1ae45c05baddea923d2976613
ismailouzy <ismailouzal@gmail.com>
2025-07-07 22:23:04 +0100

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,7 +26,6 @@ void print_times_table(int n)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(' ');
 					if (prod >= 100)
 					{
 						_putchar(prod / 100 + '0');
@@ -35,6 +34,7 @@ void print_times_table(int n)
 					}
 					else if (prod >= 10)
 					{
+						_putchar(' ');
 						_putchar(prod / 10 + '0');
 						_putchar(prod % 10 + '0');
 					}
