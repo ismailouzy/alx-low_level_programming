c72bf52fcef072a29c7d2efda897ca2ae753d195
ismailouzy <ismailouzal@gmail.com>
2025-07-07 22:50:51 +0100

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,40 +13,37 @@ void print_times_table(int n)
 
 	if (n < 0  || n > 15)
 		return;
-	else
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			prod = i * j;
+			if (j == 0)
+				_putchar(prod + '0');
+			else
 			{
-				prod = i * j;
-				if (j == 0)
-					_putchar(prod + '0');
+				_putchar(',');
+				_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar(prod / 100 + '0');
+					_putchar((prod / 10) % 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
+				else if (prod >= 10)
+				{
+					_putchar(' ');
+					_putchar(prod / 10 + '0');
+					_putchar(prod % 10 + '0');
+				}
 				else
 				{
-					_putchar(',');
 					_putchar(' ');
-					if (prod >= 100)
-					{
-						_putchar(prod / 100 + '0');
-						_putchar((prod / 10) % 10 + '0');
-						_putchar(prod % 10 + '0');
-					}
-					else if (prod >= 10)
-					{
-						_putchar(' ');
-						_putchar(prod / 10 + '0');
-						_putchar(prod % 10 + '0');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(prod + '0');
-					}
+					_putchar(' ');
+					_putchar(prod + '0');
 				}
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
