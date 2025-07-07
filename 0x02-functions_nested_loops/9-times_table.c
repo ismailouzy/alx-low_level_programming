7aaf9ae448e2e08fbc66c32c0d2a2f2ba8054771
ismailouzy <ismailouzal@gmail.com>
2025-07-07 16:11:15 +0100

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,7 +12,10 @@ void times_table(void)
 		for (j = 0; j < 10; j++)
 		{
 			if ((j * i) < 10)
+			{
 				_putchar((j * i) + '0');
+				_putchar(' ');
+			}
 			else
 			{
 				_putchar(((j * i) / 10) + '0');
