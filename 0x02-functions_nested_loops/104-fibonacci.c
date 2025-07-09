10bfdaae0ebbff79c774eee1a04cd0fbafbaaa97
ismailouzy <ismailouzal@gmail.com>
2025-07-09 01:04:16 +0100

    fibonacci with putchar

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_num - print the number digit by digit using _putchar
+ * @n: the unsigned number parameter
+ *
+ * main - Entry point
+ *
+ * Return: 0 on success.
+ */
+
+void print_num(unsigned long n);
+
+int main(void)
+{
+	unsigned long a = 1, b = 2, next;
+	int i;
+
+	for (i = 0; i < 98; i++)
+	{
+		if (i == 0)
+			_putchar(a + '0');
+		else if (i == 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(b + '0');
+		}
+		else
+		{
+			next = a + b;
+			_putchar(',');
+			_putchar(' ');
+			print_num(next);
+			a = b;
+			b = next;
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
+
+void print_num(unsigned long n)
+{
+	if (n / 10)
+		print_num(n / 10);
+	_putchar(n % 10 + '0');
+}
