8f0122d6487bc85b9225e4efd12c802a71655150
ismailouzy <ismailouzal@gmail.com>
2025-07-11 19:42:42 +0100

    print number putchar

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints a integer
+ * @n: the number to print
+ */
+
+void print_number(int n)
+{
+	int div = n / 10;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		n = n * -1;
+		print_number(n / 10);
+		_putchar(n % 10 + '0');
+	}
+	else if (n > 0)
+	{
+		if (div > 0)
+			print_number(div);
+		_putchar(n % 10 + '0');
+	}
+	else
+		_putchar(48);
+}
