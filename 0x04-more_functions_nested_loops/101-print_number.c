14e2d3ef6beacd245cb09aa5e9a7f1bc3b51c73f
ismailouzy <ismailouzal@gmail.com>
2025-07-11 19:46:53 +0100

    print number putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,8 +14,8 @@ void print_number(int n)
 	{
 		_putchar(45);
 		num = -n;
-		print_number(n / 10);
-		_putchar(n % 10 + '0');
+		print_number(num / 10);
+		_putchar(num % 10 + '0');
 	}
 	else if (n > 0)
 	{
