2b935de7311595644f4af7fa22a3136c2e3f40c4
ismailouzy <ismailouzal@gmail.com>
2025-07-11 12:51:14 +0100

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,7 +14,7 @@ int main(void)
 	{
 		if (i != 100)
 			if (i % 3 == 0 && i % 5 == 0)
-				printf("Fizz Buzz ");
+				printf("FizzBuzz ");
 			else if (i % 3 == 0)
 				printf("Fizz ");
 			else if (i % 5 == 0)
