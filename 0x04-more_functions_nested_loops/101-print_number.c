2f074cf497f4d04bcd3c89ba27d4bdeafea4db35
ismailouzy <ismailouzal@gmail.com>
2025-07-11 21:39:58 +0100

    print number putchar

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,24 @@
 
 void print_number(int n)
 {
+	int div = n / 10;
 	unsigned int num;
+
+	num = n;
 
 	if (n < 0)
 	{
 		_putchar(45);
 		num = -n;
+		print_number(num / 10);
+		_putchar(num % 10 + '0');
+	}
+	else if (n > 0)
+	{
+		if (div > 0)
+			print_number(div);
+		_putchar(num % 10 + '0');
 	}
 	else
-	{
-		num = n;
-	}
-
-	if (num / 10)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+		_putchar(num % 10 + '0');
 }
