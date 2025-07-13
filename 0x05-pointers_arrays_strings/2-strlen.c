656f1bb91ce0d9e30b790a13def9fc2ffabf832d
ismailouzy <ismailouzal@gmail.com>
2025-07-13 19:56:35 +0100

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen - calculates the length of a string
+ * @s: the pointer to the string
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	while(*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return count;
+}
+	
