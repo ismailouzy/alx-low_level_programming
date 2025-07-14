9bf9bc10b421868eb8d5d6adcaf1f51bc6b37b06
ismailouzy <ismailouzal@gmail.com>
2025-07-14 18:55:48 +0100

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,14 +7,20 @@
 
 void puts_half(char *str)
 {
-	int i = 0, j;
+	int i = 0, j, ag = 0;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
 
-	for (j = (i / 2) + 1; str[j] != '\0'; j++)
+	if(i % 2 == 0)
+		ag = i / 2;
+	else
+		ag = (i - 1) / 2;
+		 
+
+	for (j = ag; str[j] != '\0'; j++)
 	{
 		_putchar(str[j]);
 	}
