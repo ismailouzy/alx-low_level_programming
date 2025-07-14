22e982ec851af6649c6506af3c0a313efa0c2cc5
ismailouzy <ismailouzal@gmail.com>
2025-07-14 21:27:26 +0100

    strcpy implementation

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcpy - strcpy implementation (copies a string to a buffer)
+ * @dest: the pointer to the buffer
+ * @src: the pointer to the string to copy
+ *
+ * Return: the pointer to the dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
