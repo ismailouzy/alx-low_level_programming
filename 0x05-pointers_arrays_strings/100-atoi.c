6d4b610295ed1677fcf964a9d393c243edffeda5
ismailouzy <ismailouzal@gmail.com>
2025-07-15 14:56:57 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,18 +10,28 @@
 
 int _atoi(char *s)
 {
-	int i, num = 0, sign = 1;
+	int i, num = 0, sign = 1, number_started = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 48 && s[i] <= 57)
+		{
+			if (num > (2147483647 - (s[i] - 48)) / 10)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
 			num = num * 10 + (int)s[i] - 48;
-		else if (s[i] == 45)
-			sign = sign * (-1);
-		else if (s[i] == 43)
-			sign = sign * (+1);
-		else if (s[i - 1] >= 48 && s[i - 1] <= 57 && (s[i] > 57 || s[i] < 48))
+			number_started = 1;
+		}
+		else if (number_started == 1)
 			break;
+		else if (s[i] == 45)
+			sign *= -1;
+		else if (s[i] == 43)
+			continue;
 	}
 
 	return (num * sign);
--- 0x05-pointers_arrays_strings/100-main.c
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -10,21 +10,15 @@ int main(void)
 {
     int nb;
 
-    nb = _atoi("98");
+    nb = _atoi("   +--+--+-123xyz");
     printf("%d\n", nb);
-    nb = _atoi("-402");
+    nb = _atoi("abc");
     printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
+    nb = _atoi("--2147483647");
     printf("%d\n", nb);
-    nb = _atoi("214748364");
+    nb = _atoi("-2147483648");
     printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    nb = _atoi("++1-2");
     printf("%d\n", nb);
     return (0);
 }
