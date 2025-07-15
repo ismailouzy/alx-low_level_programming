4710089464e96082c240caf0211c8966a8a39d1b
ismailouzy <ismailouzal@gmail.com>
2025-07-15 15:34:04 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,20 +10,21 @@
 
 int _atoi(char *s)
 {
-	int i, num = 0, sign = 1, number_started = 0;
+	int i, num = 0, sign = 1, digit = 0, number_started = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 48 && s[i] <= 57)
 		{
-			if (num > (2147483647 - (s[i] - 48)) / 10)
+			digit = s[i] - 48;
+			if (num > (2147483647 - digit) / 10)
 			{
 				if (sign == 1)
 					return (2147483647);
 				else
 					return (-2147483648);
 			}
-			num = num * 10 + (int)s[i] - 48;
+			num = num * 10 + digit;
 			number_started = 1;
 		}
 		else if (number_started == 1)
